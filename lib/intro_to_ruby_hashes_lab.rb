79e691e7cfa5ac389389fc424128247ede3f5747
Matine temory <temorymatine@gmail.com>
2020-05-18 03:52:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,10 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ hash = {:name => "Grace Hopper"} # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  hash = {:id => }# return a hash with a key :id assigned to the provided number
 end
